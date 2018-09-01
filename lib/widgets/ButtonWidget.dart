e20979cdfaf611f2420aa79b66b0da28bb73c9c4
wuzhe213 <wuzhe213@163.com>
2018-09-01 22:26:50 +0800

    show dialog

--- lib/widgets/ButtonWidget.dart
+++ lib/widgets/ButtonWidget.dart
@@ -1,15 +1,27 @@
 import 'package:flutter/material.dart';
 
 class ButtonWidget extends StatelessWidget {
+  BuildContext widgetContext;
+
   @override
   Widget build(BuildContext context) {
+    widgetContext = context;
+
     return Center(
       child: RaisedButton(
-        onPressed: () {
-          print('pressed');
-        },
+        onPressed: _buttonPressHandler,
         child: Text('Press Me'),
       ),
+    );
+  }
+
+  _buttonPressHandler() {
+    print('pressed');
+    showDialog(
+      context: widgetContext,
+      builder: (context) {
+        return AlertDialog(content: Text('hi'),);
+      },
     );
   }
 }
